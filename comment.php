8a5abbf6e9ee7086d1d0cd27d956f4dff42944be
Sean Stephenson <seanmichaelstephenson@gmail.com>
2012-03-10 23:35:04 -0800

    add basic comments

--- comment.php
+++ comment.php
@@ -1,22 +1,25 @@
 <?php
 
-  $clean = array();
-  $html = array();
+  /* Useful Default Variables */
 
+  $clean = array(); // assignment occurs inside if statement
+  $html  = array(); // assign by passing $clean to htmlentities
 
   /* Filter Input ($name, $comment) */
 
   if ( (ctype_alnum($_POST['name'])) && (ctype_alnum($_POST['comment'])) ) {
-    $clean['name'] = $_POST['name'];
+    $clean['name']    = $_POST['name'];
     $clean['comment'] = $_POST['comment'];
   }
 
+  /* Generate Output */
 
-  $html['name'] = htmlentities($clean['name'], ENT_QUOTES, 'UTF-8');
+  $html['name']    = htmlentities($clean['name'], ENT_QUOTES, 'UTF-8');
   $html['comment'] = htmlentities($clean['comment'], ENT_QUOTES, 'UTF-8');
 
+  /* Display */
 
-  echo "<p>{$html['name']} writes:<br />";
-  echo "<blockquote>{$html['comment']}</blockquote></p>";
+  echo "<p><b>name: </b>{$html['name']}</p>";
+  echo "<p><b>comment: </b>{$html['comment']}</p>";
 
 ?>
--- index.php
+++ index.php
@@ -3,10 +3,9 @@
   </head>
   <body>
     <form action="comment.php" method="POST" />
-      <p>
-        Name:<br/><input type="text" name="name" /><br />
-        Comment:<br/><textarea name="comment" rows="10" cols="60"></textarea><br />
-        <input type="submit" value="Add Comment" /></p>
+      <p>Name:<br/><input type="text" name="name" /></p>
+      <p>Comment:<br/><textarea name="comment" rows="10" cols="60"></textarea></p>
+      <p><input type="submit" value="Add Comment" /></p>
     </form>
   </body>
 </html>
