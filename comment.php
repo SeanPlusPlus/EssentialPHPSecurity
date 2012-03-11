c593c8999fa8732bd0960b7a098ee7277e4a7a40
Sean Stephenson <seanmichaelstephenson@gmail.com>
2012-03-11 00:35:07 -0800

    add function to check for valid email address

--- comment.php
+++ comment.php
@@ -1,14 +1,89 @@
 <?php
 
+
+  /*
+  Validate an email address.
+  Provide email address (raw input)
+  Returns true if the email address has the email
+  address format and the domain exists.
+  */
+
+  function validEmail($email)
+  {
+    $isValid = true;
+    $atIndex = strrpos($email, "@");
+    if (is_bool($atIndex) && !$atIndex)
+    {
+      $isValid = false;
+    }
+    else
+    {
+      $domain = substr($email, $atIndex+1);
+      $local = substr($email, 0, $atIndex);
+      $localLen = strlen($local);
+      $domainLen = strlen($domain);
+      if ($localLen < 1 || $localLen > 64)
+      {
+        // local part length exceeded
+        $isValid = false;
+      }
+      else if ($domainLen < 1 || $domainLen > 255)
+      {
+        // domain part length exceeded
+        $isValid = false;
+      }
+      else if ($local[0] == '.' || $local[$localLen-1] == '.')
+      {
+        // local part starts or ends with '.'
+        $isValid = false;
+      }
+      else if (preg_match('/\\.\\./', $local))
+      {
+        // local part has two consecutive dots
+        $isValid = false;
+      }
+      else if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain))
+      {
+        // character not valid in domain part
+        $isValid = false;
+      }
+      else if (preg_match('/\\.\\./', $domain))
+      {
+        // domain part has two consecutive dots
+        $isValid = false;
+      }
+      else if (!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/',
+                str_replace("\\\\","",$local)))
+      {
+        // character not valid in local part unless
+        // local part is quoted
+        if (!preg_match('/^"(\\\\"|[^"])+"$/',
+            str_replace("\\\\","",$local)))
+        {
+            $isValid = false;
+        }
+      }
+      if ($isValid && !(checkdnsrr($domain,"MX") || checkdnsrr($domain,"A")))
+      {
+        // domain not found in DNS
+        $isValid = false;
+      }
+    }
+    return $isValid;
+  }
+
   /* Useful Default Variables */
 
   $clean = array(); // assignment occurs inside if statement
-  $html  = array(); // assign by passing $clean to htmlentities
+  $html  = array(); // to assign, pass $clean to htmlentities
 
-  /* Filter Input ($name, $comment) */
+  /* Filter Input */
 
-  if ( (ctype_alnum($_POST['name'])) && (ctype_alnum($_POST['comment'])) ) {
-    $clean['name']    = $_POST['name'];
+  if ( validEmail($_POST['name']) )  {
+    $clean['name'] = $_POST['name'];
+  }
+
+  if ( ctype_alnum($_POST['comment']) )  {
     $clean['comment'] = $_POST['comment'];
   }
 
@@ -21,5 +96,13 @@
 
   echo "<p><b>name: </b>{$html['name']}</p>";
   echo "<p><b>comment: </b>{$html['comment']}</p>";
+
+  /* How Not to Do It */
+
+  //$name = $_POST['name'];
+  //$comment = $_POST['comment'];
+
+  //echo  "<p><b>name: </b>$name</p>";
+  //echo "<p><b>comment: </b>$comment</p>";
 
 ?>
